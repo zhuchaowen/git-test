1b934d2fddc133bcf6ce8191f14ffd7ea02db6c0
zhuchaowen <zhuchaowen20@gmail.com>
2025-03-15 00:09:18 +0800

    Branch Commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("Hello, Git!");
         System.out.println("Hello, IDEA!");
+        System.out.println("Hello, Branch!");
     }
 }
